7018108e3217ec606beec0a0036f62e8c2f32928
SuperTigim <plenum@naver.com>
2016-03-03 00:18:27 +0900

    update utils
    1. temporally add logger functions
    2. define maximum number of session

--- server/lib/utils.hpp
+++ server/lib/utils.hpp
@@ -1,7 +1,40 @@
 #pragma once 
 
 #include <boost/thread/thread.hpp>
+#include <boost/asio.hpp>
+
 #include <iostream>
+
+//#define DEBUG
+
+// const
+//////////////////////////////////////////////////////////////////
+unsigned short MAX_SESSION_NUM = 65535;
+
+// logger
+//////////////////////////////////////////////////////////////////
+namespace logger {
+
+	inline void info(const std::string& log) {
+#ifdef _DEBUG_
+		std::cout << "[INFO] " << log << std::endl;
+#endif //end of _DEBUG_
+	}
+
+	inline void warning(const std::string& log) {
+#ifdef _DEBUG_
+		std::cout << "[WARNING] " << log << std::endl;
+#endif //end of _DEBUG_		
+	}
+
+	inline void error(const std::string& log) {
+#ifdef _DEBUG_
+		std::cout << "[ERROR] " << log << std::endl;
+#endif //end of _DEBUG_		
+	}
+
+} // end of logger 
+
 
 
 // end of code 
