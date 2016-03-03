aec92e9e800f0ea3b152278a6c70b3830df46eeb
SuperTigim <plenum@naver.com>
2016-03-03 23:17:34 +0900

    session class update
    implement some member function in session class

--- server/lib/session.cpp
+++ server/lib/session.cpp
@@ -1,2 +1,124 @@
 #include "session.hpp"
+#include "server_ctrl.hpp"
+#include <boost/make_shared.hpp>
+
+
+// session constructor
+//////////////////////////////////////////////////////////////////
+session::session( boost::asio::io_service& io_service, unsigned short session_id)
+	: session_id_(session_id)
+	, session_stat_(SS_CLOSE)
+{
+	socket_ = boost::make_shared<boost::asio::ip::tcp::socket>(boost::ref(io_service));
+}
+
+/*
+session::session (
+	boost::asio::io_service& ios,
+	const boost::asio::ip::tcp::endpoint& endpoint
+	)
+		:socket_(boost::shared_ptr<boost::asio::ip::tcp::socket>(
+			boost::ref(ios)))
+{
+	socket_->connect(endpoint);
+}
+*/
+
+// open session with client  
+//////////////////////////////////////////////////////////////////
+bool session::open() {
+
+	// socket has to be opened first before doing this. 
+	if(!this->socket().is_open()) return false;
+
+	// update session statement into SS_OPEN
+	set_session_stat(SS_OPEN);
+	
+	return true;
+}
+
+void session::shutdown() {
+	socket_->shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+	socket_->close();
+
+	set_session_stat(SS_CLOSE);
+}
+
+// set the buffer into io service in order to receive incoming data   
+//////////////////////////////////////////////////////////////////
+bool session::post_recv() {
+
+	// socket has to be opened first before doing this. 
+	if(!this->socket().is_open()) return false;
+
+	this->socket().async_read_some ( 
+		boost::asio::buffer(rcv_buff_), 
+		boost::bind( &session::handle_receive, 
+					this, 
+					boost::asio::placeholders::error, 
+					boost::asio::placeholders::bytes_transferred ) 
+	);
+
+	return true;
+}
+
+//  
+//////////////////////////////////////////////////////////////////
+void session::handle_receive( const boost::system::error_code& error, std::size_t bytes_transferred ) {
+	if( error )
+	{
+		if( error == boost::asio::error::eof )
+		{
+			logger::info("remote peer closed the connection");
+		}
+		else 
+		{
+			logger::error("socket error is occured!!!");
+		}
+
+		server_ctrl::get().release_session( session_id_ );
+	}
+	else
+	{
+		/* do somehing here */
+		process_packet(bytes_transferred);
+
+		// reset io service to receive data
+		post_recv();
+	}
+}
+
+//  
+//////////////////////////////////////////////////////////////////
+bool session::post_send(const char* data, const unsigned short size) {
+
+	// socket has to be opened first before doing this. 
+	if(!this->socket().is_open()) return false;
+
+	boost::asio::async_write( this->socket(), boost::asio::buffer( data, size ),
+							 	boost::bind( &session::handle_send, this,
+								boost::asio::placeholders::error,
+								boost::asio::placeholders::bytes_transferred )
+							);
+
+	return true;
+}
+
+//  
+//////////////////////////////////////////////////////////////////
+void session::handle_send(const boost::system::error_code& error, std::size_t bytes_transferred) {
+
+	/* do nothing at the moment */
+	logger::info("do nothing at the moment");
+}
+
+//  
+//////////////////////////////////////////////////////////////////
+void session::process_packet(std::size_t bytes_transferred) {
+
+	/* example: echo server */
+	logger::info("process packet");
+
+
+}
 
--- server/lib/session.hpp
+++ server/lib/session.hpp
@@ -10,23 +10,7 @@ class session /*: public boost::enalbed_shread_from_this<session>*/ {
 
 //---------------------- Member Functions ----------------------//
 public:
-	explicit session( boost::shared_ptr<boost::asio::ip::tcp::socket> socket)
-		: socket_(socket)
-		, session_id_(MAX_SESSION_NUM)
-	{}
-
-	/*
-	explicit session (
-		boost::asio::io_service& ios,
-		const boost::asio::ip::tcp::endpoint& endpoint
-		)
-			:socket_(boost::shared_ptr<boost::asio::ip::tcp::socket>(
-				boost::ref(ios)))
-	{
-		socket_->connect(endpoint);
-	}
-	*/
-
+	explicit session( boost::asio::io_service& io_service, unsigned short session_id);
 	virtual ~session() {}
 
 	// return the socket which this connection gets
@@ -36,42 +20,45 @@ public:
 
 	// return session ID
 	unsigned short sesson_id() {
-
-		if(session_id_ == MAX_SESSION_NUM) {
-			logger::error("session is not open!!!!");
-		}
-		
 		return session_id_;
 	}
 
-	template<class Functor>
-	void async_read(
-		const boost::asio::mutable_buffers_1& buf,
-		const Functor& f,
-		std::size_t at_least_bytes
-		) const 
-	{
-		//boost::asio::async_read(*socket_, buf, boost::asio::trasnfer_at_least(at_least_bytes), f);
+	unsigned short get_session_stat(){
+		return session_stat_;
+	}
+	void set_waiting_mode(){
+		set_session_stat(SS_WAITING);
 	}
 
-	template<class Functor>
-	void async_write (
-		const boost::asio::const_buffers_1& buf,
-		const Functor& f
-		) const
-	{
-		//boost::asio::async_write(*socket_, buf, f);
+	bool open();												// initailization after socket is on
+	void shutdown();
+
+	bool post_recv();											// preparation for receiving data from client 
+	bool post_send(const char* data, const unsigned short size);// send data to client
+	//bool post_send(const boost::asio::const_buffers_1& buff);
+
+	
+protected:
+	// internal function to change session statement
+	inline void set_session_stat(unsigned short new_session_stat) {	
+		session_stat_ = new_session_stat;
 	}
 
-	void shutdown() const {
-		socket_->shutdown(boost::asio::ip::tcp::socket::shutdown_both);
-		socket_->close();
-	}
+	virtual void process_packet(std::size_t bytes_transferred);	// do process packet received from client 
+
+	// handle received data after receiving data is done
+	void handle_receive( const boost::system::error_code& error, std::size_t bytes_transferred );
+
+	// handle sonthing after sending data if needed
+	void handle_send(const boost::system::error_code& error, std::size_t bytes_transferred);
 
 //---------------------- Member Variables ----------------------//	
 protected:
 	boost::shared_ptr<boost::asio::ip::tcp::socket> socket_;
 	unsigned short session_id_;									// max_num: 65,535
-	//tcp_listener tcp_listener_;								//
+	
+	std::array<char, MAX_RECEIVE_BUFFER_LEN> rcv_buff_;
+
+	unsigned short session_stat_;
 
 };
