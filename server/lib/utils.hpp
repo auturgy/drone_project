d50a99f50937c645a732fdab016e455adf5958f5
SuperTigim <plenum@naver.com>
2016-03-03 23:16:21 +0900

    add const variables
    to manage session statement

--- server/lib/utils.hpp
+++ server/lib/utils.hpp
@@ -1,17 +1,26 @@
 #pragma once 
 
+// include 
+//////////////////////////////////////////////////////////////////
 #include <boost/thread/thread.hpp>
 #include <boost/asio.hpp>
-
 #include <iostream>
-
-//#define DEBUG
 
 // const
 //////////////////////////////////////////////////////////////////
-unsigned short MAX_SESSION_NUM = 65535;
+const unsigned short SESSION_NUM_LIMIT = 65535;
+const unsigned short MAX_SESSION_COUNT = 100;					// shold be smaller than SESSION_NUM_LIMIT
 
-// logger
+const unsigned short MAX_RECEIVE_BUFFER_LEN = 512;				// how about MAX MTU size 1500? 
+
+// session statement 
+const unsigned short SS_CLOSE = 0;								// session is off 
+const unsigned short SS_WAITING = 1;							// when waiting for accepting 
+const unsigned short SS_OPEN = 2;								// session is on
+const unsigned short SS_MAX = 3;								// maximum count 
+
+
+// namespace logger
 //////////////////////////////////////////////////////////////////
 namespace logger {
 
@@ -32,9 +41,6 @@ namespace logger {
 		std::cout << "[ERROR] " << log << std::endl;
 #endif //end of _DEBUG_		
 	}
-
 } // end of logger 
-
-
 
 // end of code 
