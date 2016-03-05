ee44e8d11bba5a61138d74a251e0df71539724de
SuperTigim <plenum@naver.com>
2016-03-05 21:42:23 +0900

    update test main
    run tcp server based on boss Casio

--- server/lib/main.cpp
+++ server/lib/main.cpp
@@ -3,7 +3,12 @@
 
 int main() {
 
-	std::cout << "CPU number: " << server_ctrl::get().get_number_of_process() << std::endl;
+	//std::cout << "CPU number: " << server_ctrl::get().get_number_of_process() << std::endl;
 
+	server_ctrl::get().init();
+	server_ctrl::get().start();
+	
+	server_ctrl::get().stop();
+	getchar();
 	return 0;
 }
