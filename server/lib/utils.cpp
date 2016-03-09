07fc1bfdf4e2e23ddb4d93394bbed43238b843f0
SuperTigim <plenum@naver.com>
2016-03-09 15:59:02 +0900

    update logger class

---
+++ server/lib/utils.cpp
@@ -0,0 +1,8 @@
+#include "utils.hpp"
+
+// Logger instance 
+//////////////////////////////////////////////////////////////////
+std::unique_ptr<Logger> Logger::instance_;
+
+
+// end of file 
--- server/lib/utils.hpp
+++ server/lib/utils.hpp
@@ -21,28 +21,81 @@ const unsigned short SS_WAITING = 1;							// when waiting for accepting
 const unsigned short SS_OPEN = 2;								// session is on
 const unsigned short SS_MAX = 3;								// maximum count 
 
-
-// namespace logger
+// logger class
 //////////////////////////////////////////////////////////////////
-namespace logger {
+class Logger
+{
+//---------------------- Member Functions ----------------------//	
+private:
+	Logger(std::ostream &o = std::cout):m_file(o){};
 
-	inline void info(const std::string& log) {
-#ifdef _DEBUG_
-		std::cout << "[INFO] " << log << std::endl;
-#endif //end of _DEBUG_
+	static inline Logger& get(){
+		Logger *p = instance_.get();
+	
+		if(!p) {
+			instance_.reset(new Logger);
+			p = instance_.get();
+		}
+		return *p;
 	}
 
-	inline void warning(const std::string& log) {
-#ifdef _DEBUG_
-		std::cout << "[WARNING] " << log << std::endl;
-#endif //end of _DEBUG_		
+public:    
+
+	static Logger& info(){
+		Logger *p =  &get();
+		
+#ifdef _DEBUG_		
+		*p << "[INFO] ";
+#endif /*_DEBUG_*/
+
+		return *p;
 	}
 
-	inline void error(const std::string& log) {
-#ifdef _DEBUG_
-		std::cout << "[ERROR] " << log << std::endl;
-#endif //end of _DEBUG_		
+	static Logger& warning(){
+		Logger *p =  &get();
+
+#ifdef _DEBUG_				
+		*p << "[WARNING] ";
+#endif /*_DEBUG_*/
+
+		return *p;
 	}
-} // end of logger 
+
+	static Logger& error(){
+		Logger *p =  &get();
+
+#ifdef _DEBUG_		
+		*p << "[INFO] ";
+#endif /*_DEBUG_*/		
+
+		return *p;
+	}
+
+    template <typename T>
+    Logger &operator << (const T &a) {
+
+#ifdef _DEBUG_		    	
+        m_file<<a;
+#endif /*_DEBUG_*/
+
+        return *this;
+    }
+
+    Logger &operator<<(std::ostream& (*pf) (std::ostream&)){
+
+#ifdef _DEBUG_		
+        m_file<<pf;
+#endif /*_DEBUG_*/
+
+        return *this;
+    }
+
+//---------------------- Member Functions ----------------------//	
+private:
+    std::ostream &m_file;
+    static std::unique_ptr<Logger> instance_;					// logger itself
+
+}; // end of logger class 
+
 
 // end of code 
