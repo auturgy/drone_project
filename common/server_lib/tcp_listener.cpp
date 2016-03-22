a230e4c58ed2de6b2046bc7f1c0451d7dbe34abd
SuperTigim <plenum@naver.com>
2016-03-22 21:10:33 +0900

    fix the error that causes system fault when no session is available for a new client
    - update many things related to listener
    - even if there is no session instance to handle new client, server
    does not crash.

--- common/server_lib/tcp_listener.cpp
+++ common/server_lib/tcp_listener.cpp
@@ -4,28 +4,29 @@
 
 // Post Accept methond to prepare for incoming connection 
 //////////////////////////////////////////////////////////////////
-bool tcp_listener::PostAccept() {
+bool tcp_listener::PostAccept(boost::shared_ptr<session>* ss_ptr) {
 	if(!acceptor_.is_open()) {
 		return false;
 	}
 
 	Logger::info() << "tcp_listener::PostAccept() - BEGIN" << std::endl;
 
-	boost::shared_ptr<session>& ss_ptr = server_ctrl::get().alloc_session();
-
-	if(ss_ptr == nullptr) {
-
-		Logger::warning() << "not enough session instance !!!" << std::endl;
-
-		return false;	
+	if(ss_ptr == nullptr){
+		ss_ptr = server_ctrl::get().alloc_session_p();
 	}
+	assert(ss_ptr != nullptr);
+	
+	//if(ss_ptr == nullptr) {
+	//	Logger::warning() << "not enough session instance !!!" << std::endl;
+	//	return false;	
+	//}
 
 	acceptor_.async_accept(
-		ss_ptr.get()->socket(),
+		ss_ptr->get()->socket(),
 		boost::bind(
 			&tcp_listener::handle_accept,
 			this->shared_from_this(),
-			ss_ptr.get(),
+			ss_ptr->get(),
 			boost::asio::placeholders::error
 			)
 		);
@@ -44,11 +45,11 @@ void tcp_listener::handle_accept(session* ss_ptr, const boost::system::error_cod
 		Logger::info() << "Connection is established~...." << std::endl;
 		
 		// session is working properly 
-		ss_ptr->open();
+		ss_ptr->open(port_);
 		ss_ptr->post_recv();
 		
 		// waiting for next connection
-		PostAccept();
+		//PostAccept();
 	}
 	else 
 	{
--- common/server_lib/tcp_listener.hpp
+++ common/server_lib/tcp_listener.hpp
@@ -16,20 +16,19 @@ class tcp_listener : public boost::enable_shared_from_this<tcp_listener> {
 public:
 	tcp_listener(boost::asio::io_service& io_service, unsigned short port)
 		: acceptor_(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port))
+		, port_(port)
 	{}
 
-	bool start_listening(std::size_t concur_listener_num) {									//	start listening 
-		
+	bool start_listening(std::size_t concur_listener_num) {		//	start listening 	
 		bool ret = true;
 		for( std::size_t i = 0 ; i < concur_listener_num ; i++) {
-			ret = ret & PostAccept();
+			ret = ret & PostAccept(nullptr);
 		}
-
 		return ret;
 	}
 
+	bool PostAccept(boost::shared_ptr<session>* ss_ptr);		// waiting for client connection 
 protected:
-	bool PostAccept();											// waiting for client connection 
 	void handle_accept(
 		session* ss_ptr, 
 		const boost::system::error_code& error);
@@ -37,4 +36,5 @@ protected:
 //---------------------- Member Variables ----------------------//
 protected:
 	acceptor_t acceptor_;
+	unsigned short port_;
 };
